bbf5c2244d2d0492830b1627a09f59c3a75dda0f
christopherdubeau <dubeauchristopher@fhda.edu>
2014-11-15 11:55:21 -0800

    Initial Commit

---
+++ src/program/InputData.java
@@ -0,0 +1,12 @@
+package program;
+
+/**
+ * @author Christopher Dubeau
+ * @author Ron Rayas
+ * @author Harpreet Setia
+ * @author Long Hu
+ * Date: 11/15/14
+ * Description:
+ */
+public class InputData {
+}
