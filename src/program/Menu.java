286d7beaa7d88cff473040b4d02bc4872e9eb01f
iturban7 <harpreet.setia777@gmail.com>
2014-11-16 13:39:42 -0800

    Update Menu.java

--- src/program/Menu.java
+++ src/program/Menu.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 package program;
 
 /**
@@ -8,12 +9,56 @@ package program;
  * Date: 11/15/14
  * Description:
  */
+
+
 public class Menu {
+		Scanner keyboard = new Scanner(System.in);
+		
+		
+		 System.out.println("-- Actions --");
+		    System.out.println(
+		        "Select an option: \n" +
+		        "  1) Enter a new person\n" +
+		        "  2) Print the contact list\n" +
+		        "  3) Retreive a persons information by last name \n" +
+		        "  4) Retrieve a person's information by email address\n " +
+		        " 5) Retrieve all people who live in a given zip code \n" +
+		        "  0) Exit menu"
+		    );
+		    
+		    int selection = keyboard.nextInt();
+		    keyboard.nextLine();
+		    
+		    
+		    	
+	
+		    
+		    while (selection > 0 && selection < 6){
+		    	switch (selection){
+		    	case 1:
+		    		//Enter new person
+		    		break;
+		    	case 2:
+		    		// Print the information
+		    		break;
+		    	case 3: 
+		    		// Retrieve info by last name
+		    		break;
+		    	case 4:
+		    		//Retrieve info by email
+		    		break;
+		    	case 5: 
+		    		//Retrieve info by zip code
+		    		break;
+		    	case 0: 
+		    		selection = 6;
+		    		break;
+		    	default:
+		    		 System.out.println("Invalid selection.");
+		    		 break;
+		    	}
+		    	
+		    }
+	}
 
-    /**
-     * This method will display a menu prompt to the user
-     */
-    public void displayMenu(){
-
-    }
 }
