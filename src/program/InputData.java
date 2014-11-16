173ba7b3107da65d4bc1009b3468ecb5303f8bc3
christopherdubeau <dubeauchristopher@fhda.edu>
2014-11-16 12:33:33 -0800

    fixed input data file

--- src/program/InputData.java
+++ src/program/InputData.java
@@ -1,33 +1,33 @@
+package program;
+/**
+ * @author Christopher Dubeau
+ * @author Ron Rayas
+ * @author Harpreet Setia
+ * @author Long Xu
+ * Date: 11/16/14
+ * Description:
+ * Define a reuseable class InputDate.
+ * One object of class InputDate reads and stores the users inputs.
+ */
 
-	/**
- 	 * @author Christopher Dubeau
-	 * @author Ron Rayas
- 	 * @author Harpreet Setia
-	 * @author Long Xu
-	 * Date: 11/16/14
-	 * Description:
-	 * Define a reuseable class InputData.
-	 * One object of class InputDate reads and stores the users inputs.
-	 */
+import java.util.Scanner;
+public class  InputData {
 
-	import java.util.Scanner;
-	public class  InputData{ 
+	private String firstName; //FirstName is a data member
+	private String lastName; // LastName is a data member
+	private String streetAddress; // StreetAddress is a data member
+	private String emailAddress;  // EmailAddress is a data member
+	private int phoneNumber=0; //PhoneNumber is a data member
+	private String notes;  //Notes is a data member
 
-		private String firstName; //FirstName is a data member
-		private String lastName; // LastName is a data member
-		private String streetAddress; // StreetAddress is a data member
-		private String emailAddress;  // EmailAddress is a data member
-		private int phoneNumber=0; //PhoneNumber is a data member
-		private String notes;  //Notes is a data member
-		
-		public void InputData (){   //Create an method which reads and stores readers' inputs
-			Scanner scanner = new Scanner(System.in); 
-			 firstName = scanner.next();
-			 lastName = scanner.next();
-			 streetAddress = scanner.next();
-			 emailAddress = scanner.next();
-			 phoneNumber = scanner.nextInt();
-			 notes = scanner.next();
-			 }
-	}
+	public void InputDate (){   //Creat an object which reads and stores readers' inputs
+		Scanner scanner = new Scanner(System.in);
+		 firstName = scanner.next();
+		 lastName = scanner.next();
+		 streetAddress = scanner.next();
+		 emailAddress = scanner.next();
+		 phoneNumber = scanner.nextInt();
+		 notes = scanner.next();
+		 }
 }
+
