a37d2a2965345449d37ff60b38e931b7976d3f16
christopherdubeau <dubeauchristopher@fhda.edu>
2014-11-16 14:05:56 -0800

    updated methods

--- src/program/ContactList.java
+++ src/program/ContactList.java
@@ -12,6 +12,41 @@ public class ContactList {
 
     public static void main(String ars[]){
 
+        Menu menu = new Menu();
+        InputData inputData = new InputData();
+
+        int selection = menu.optionMenu();
+        ReadData readData = new ReadData();
+
+
+        while(selection >0 && selection < 6)
+        {
+            switch (selection) {
+                case 1:
+                    //Enter new person
+                    break;
+                case 2:
+                    // Print the information
+                    break;
+                case 3:
+                    // Retrieve info by last name4
+                    break;
+                case 4:
+                    //Retrieve info by email
+                    break;
+                case 5:
+                    //Retrieve info by zip code
+                    break;
+                case 0:
+                    selection = 6;
+                    break;
+                default:
+                    System.out.println("Invalid selection.");
+                    break;
+            }
+
+        }
+
     }
 
 }
