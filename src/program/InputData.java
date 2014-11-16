8d9d4507bb088661265921986a90d8525502549e
LongXu <xulonggg@gmail.com>
2014-11-16 09:51:27 -0800

    Update InputData.java

--- src/program/InputData.java
+++ src/program/InputData.java
@@ -7,6 +7,26 @@ package program;
  * @author Long Hu
  * Date: 11/15/14
  * Description:
- */
-public class InputData {
+ * Define a reuseable class InputDate.
+ * One object of class InputDate reads and stores the users inputs.
+ */ 
+ import java.util.Scanner; // import Scanner 
+public class  InputData { 
+
+	private String FirstName; //FirstName is a data member
+	private String LastName; // LastName is a data member
+	private String StreetAddress; // StreetAddress is a data member
+	private String EmailAddress;  // EmailAddress is a data member
+	private int PhoneNumber=0; //PhoneNumber is a data member
+	private String Notes;  //Notes is a data member
+	
+	public void InputDate (){   //Creat an object which reads and stores readers' inputs
+		Scanner scanner = new Scanner(System.in); 
+		 FirstName = scanner.next();
+		 LastName = scanner.next();
+		 StreetAddress = scanner.next();
+		 EmailAddress = scanner.next();
+		 PhoneNumber = scanner.nextInt();
+		 Notes = scanner.next();
+		 }
 }
