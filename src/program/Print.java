761137e190e321d3c3f7b855b1cdd1e977b1227d
iturban7 <harpreet.setia777@gmail.com>
2014-11-16 12:48:09 -0800

    Update Print.java

--- src/program/Print.java
+++ src/program/Print.java
@@ -1,3 +1,4 @@
+import java.util.List;
 package program;
 
 /**
@@ -8,5 +9,28 @@ package program;
  * Date: 11/15/14
  * Description:
  */
+
+/*
+ * This Print class will print either the first name, last name, street address,
+ * email,notes that the user inputs. Or it will print all of them.
+ */
 public class Print {
+	private List data;
+	
+	public Print(List readData){
+		data = readData; 
+		
+	}
+	
+	public String printData() {
+		return null;
+		//for loop
+		
+		//for(i=0; i<Data.length;i++)
+			//Data[i];
+	}
+	
+	
+	
+
 }
